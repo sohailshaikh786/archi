763bc29a05c90cf294faaa1a7e3efb79351600e9
sohail <sohailhacker7786@gmail.com>
2021-06-06 06:58:47 +0530

    new edit

--- archi/settings.py
+++ archi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sk@47u4+b939*^+qy0$y0ealpa2-&9c7s)1les$z9mqbg75zh5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
